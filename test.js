a89388cde1c462c3f2d06e9613d732889a700980
Rangsan AWB <rangsan.awb@gmail.com>
2021-07-13 18:39:50 +0700

    log

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function lsExample() {
-  const { stdout, stderr } = await exec('darknet /?');
+  const { stdout, stderr } = await exec('darknet detector test D:/11_Conda/darknet/data2/obj.data D:/11_Conda/darknet/data2/yolov4-class84.cfg D:/11_Conda/darknet/data2/backup/yolov4-class84_last.weights D:/11_Conda/darknet/data2/obj/A001/A001_7.jpg -dont_show -thresh 0.5 > D:/11_Conda/darknet/data2/logs/log.txt');
   console.log('stdout:', stdout);
   console.error('stderr:', stderr);
 }
